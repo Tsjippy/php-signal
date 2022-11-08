b0df70bb740e3ddb01f8483bbfb17dfd0cdbeac8
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2022-11-08 06:23:59 +0100

    Update signal-daemon.php

--- src/signal-daemon.php
+++ src/signal-daemon.php
@@ -9,29 +9,17 @@ if(!empty($argv) && count($argv) == 2){
         return;
     }
 
-    // load wp
-    ob_start();
-    define( 'WP_USE_THEMES', false ); // Do not use the theme files
-    define( 'COOKIE_DOMAIN', false ); // Do not append verify the domain to the cookie
-
-    require(__DIR__."/../../../../../../../wp-load.php");
-    require_once ABSPATH . WPINC . '/functions.php';
-
-    $discard = ob_get_clean();
-
+    
     /* Remove the execution time limit */
     set_time_limit(0);
 
-    include_once __DIR__.'/../php/__module_menu.php';
-    include_once __DIR__.'/../php/classes/Signal.php';
+    include_once __DIR__.'/Signal.php';
 
     $signal = new SignalBus();
 
     if($data->account != $signal->phoneNumber){
         return;
     }
-
-    SIM\printArray($data);
 
     // message to group
     if(
@@ -54,25 +42,8 @@ if(!empty($argv) && count($argv) == 2){
 function getAnswer($message, $source){
     $message = strtolower($message);
 
-    //Change the user to the adminaccount otherwise get_users will not work
-    wp_set_current_user(1);
-
-    $name = false;
-    $users = get_users(array(
-        'meta_key'     => 'phonenumbers',
-    ));
-
-    foreach($users as $user){
-        $phonenumbers = get_user_meta($user->ID,'phonenumbers',true);
-        if(in_array($source,$phonenumbers)){
-            $name = $user->first_name;
-        }
-    }
-
     if($message == 'test'){
         return 'Awesome!';
-    }elseif(strpos($message, 'prayer') !== false && $name){
-        return "This is the prayer for today:\n\n".SIM\PRAYER\prayerRequest(true, $name);
     }elseif($message == 'hi' || strpos($message, 'hello') !== false){
         return "Hi $name";
     }else{
